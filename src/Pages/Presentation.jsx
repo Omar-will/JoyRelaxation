1ed1bb6751ebf201f0f0ec61b5dff94453945a4b
Omar-will <omar.bengoudifa@outlook.fr>
2024-12-03 12:40:53 +0100

    modif

--- src/Pages/Presentation.jsx
+++ src/Pages/Presentation.jsx
@@ -15,15 +15,19 @@ const PresentationPage = () => {
     <div className="text-overlay">
       <h2>À propos de Joy</h2>
       <p>
-        Originaire de la Martinique, Joy a grandi au cœur des paysages tropicaux enchanteurs, bercée par la richesse des traditions et l'énergie bienveillante de son île. Très jeune, elle s'est passionnée pour le bien-être et les soins corporels, inspirée par les pratiques ancestrales locales qui valorisent l'harmonie entre le corps et l'esprit.
+      Originaire des Antilles, Joy a grandi entourée de paysages tropicaux enchanteurs, imprégnée des traditions locales et de l'énergie bienveillante de son île. 
+      
+      Très jeune, elle s'est passionnée pour le bien-être et les soins corporels, inspirée par les pratiques ancestrales qui privilégient l'harmonie entre le corps et l'esprit. 
+      
+      En 2012, elle commence sa formation en techniques de massage et obtient son certificat d'aptitude. Dès lors, elle ouvre son premier salon de massage, qu'elle dirige avec succès jusqu'en 2016. 
+      
+      Forte de cette expérience, elle ouvre un deuxième salon, qu'elle gère de 2017 à 2019, dans les Antilles, en alliant méthodes modernes et savoirs traditionnels, afin d'offrir à sa clientèle des moments uniques de relaxation et de revitalisation.
 
-        Après avoir suivi une formation complète en techniques de massage en Martinique, elle a ouvert deux salons de massage réputés sur l'île. Elle a perfectionné son art en mêlant des méthodes modernes aux savoirs traditionnels, offrant à sa clientèle des expériences uniques de relaxation et de revitalisation.
-
-        Animée par l'envie de partager sa passion au-delà des frontières de son île natale, Joy a fait le choix audacieux de s’installer en métropole. Elle a choisi Dieppe pour y poser ses valises, séduite par son charme maritime et son ambiance chaleureuse. C'est ici qu'elle a ouvert son propre salon de massage, un espace dédié au bien-être et à la sérénité.
-
-        Chaque soin qu’elle propose est une invitation au voyage, puisant dans ses racines antillaises et son expérience professionnelle pour offrir à ses clients des moments de détente inoubliables. Sa passion pour le massage ne se limite pas à une simple profession : pour elle, c'est un véritable art de vivre, un moyen d'accompagner chaque personne vers un équilibre et une paix intérieure.
-
-        Venez découvrir son univers et laissez-vous emporter par des soins qui respirent l’exotisme et la douceur, tout en étant empreints d'une expertise reconnue.
+      Désireuse de partager sa passion au-delà de son île natale, Joy décide de s'installer en métropole pour ouvrir son propre salon de massage, un lieu dédié au bien-être et à la sérénité. 
+      
+      Chaque soin qu’elle propose devient une invitation au voyage, alliant ses racines antillaises et son expertise pour offrir des moments de détente inoubliables. Pour Joy, le massage est bien plus qu'une profession, c'est un art de vivre, une voie vers l'équilibre et la paix intérieure. 
+      
+      Venez découvrir son univers et laissez-vous emporter par des soins empreints d’exotisme, de douceur et d’une expertise reconnue.
       </p>
     </div>
   </div>
